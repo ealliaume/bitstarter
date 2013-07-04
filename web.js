af9e66448ab1c03cd4c31ac3885a8e7778d37f2d
Erwan Alliaume <erwan.alliaume@gmail.com>
2013-07-04 08:45:53 +0200

    index.html -> layout

--- web.js
+++ web.js
@@ -1,12 +1,30 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/hello', function(request, response) {
+	response.send('Hello World!');
 });
+
+
+app.get('/testToRemove', function(request, response) {
+	var k = fs.readFileSync('./index.html', 'utf8');
+	response.send( k );
+});
+
+app.get('/', function(request, response) {
+	var k = fs.readFileSync('./index.html', 'utf8');
+
+	var buffer = new Buffer(k, 'utf-8');
+	response.send( buffer.toString('utf-8') );
+});
+
+
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
